d2df3928ebd1e07d3cdd95bb09e63484b494eada
David <zdavid1@myges.fr>
2023-03-03 01:48:36 +0100

    dev

--- Extia/src/app/add-product/add-product.service.ts
+++ Extia/src/app/add-product/add-product.service.ts
@@ -9,13 +9,19 @@ import {Category} from 'src/app/models/Category';
 export class CategoryListService {
   //private getCategory = `https://clean-code.azurewebsites.net/category`;
   private getCategory = `http://localhost:8080/category`;
+  private getProduct = `http://localhost:8080/category`;
 
   constructor(private httpClient: HttpClient) {
   }
 
-   fetchAll(): Observable<any> {
-    const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*' });
-    return this.httpClient.get(this.getCategory,{headers});
+  fetchAll(): Observable<any> {
+    const headers = new HttpHeaders({'Access-Control-Allow-Origin': '*'});
+    return this.httpClient.get(this.getCategory, {headers});
+  }
+
+  fetchAllProducts(): Observable<any> {
+    const headers = new HttpHeaders({'Access-Control-Allow-Origin': '*'});
+    return this.httpClient.get(this.getCategory, {headers});
   }
 
 }
